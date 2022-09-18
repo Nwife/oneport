5cfadd2d47593d7e58e8b6afcb9922aaf7933417
Nwife <ifeeamaeze@gmail.com>
2022-09-18 05:56:46 +0100

    implemented mobile menu

--- src/component/topnav/TopNav.js
+++ src/component/topnav/TopNav.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
+
+//components
+import Sidebar from '../sidebar/Sidebar';
 
 //icons & images
 import bell from '../../assets/notification.svg';
 import avatar from '../../assets/avatar.png';
 import caret from '../../assets/caret.svg';
+import menu from '../../assets/menu.svg';
+import menuclose from '../../assets/menuclose.svg';
 
 export default function TopNav() {
   const { pathname } = useLocation()
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  const handleToggleMenu = () => {
+    setToggleMenu(false)
+  }
+
   console.log('pathname>>>', pathname)
   return (
     <div className='px-[40px] md:px-[70px] py-6 flex items-center justify-between border-b-[1px]'>
@@ -16,12 +27,21 @@ export default function TopNav() {
         </h1>
         <div className='flex items-center'>
             <img src={bell} alt="bell" />
-            <div className='flex items-center space-x-1 ml-4 md:ml-12'>
+            <div className='hidden md:flex items-center space-x-1 ml-4 md:ml-12'>
                 <img src={avatar} alt="avatar" className='w-[35px] h-[35px] md:w-[56px] md:h-[56px]' />
                 <p>Temoc</p>
                 <img src={caret} alt="" />
             </div>
-        </div>
+            <div className='mobile'>
+              {toggleMenu
+                ? <div className='md:hidden ml-3' onClick={() => setToggleMenu(false)}><img src={menuclose} alt="" /></div>
+                : <div className='md:hidden ml-3' onClick={() => setToggleMenu(true)}><img src={menu} alt="" /></div>
+              }
+              {toggleMenu && (
+                <div className='mobilemenu absolute top-[90px] w-auto right-0 bg-white p-12 shadow-lg z-10'><Sidebar handleToggleMenu={handleToggleMenu} /></div>
+              )}
+            </div>
+        </div> 
     </div>
   )
 }
